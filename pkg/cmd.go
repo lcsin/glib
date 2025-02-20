72fccbce8a6f50563ff148fcb3e6aa274790abc6
lcsin <1847@qq.com>
2025-02-20 11:28:37 +0800

    new

---
+++ pkg/cmd.go
@@ -0,0 +1,44 @@
+package pkg
+
+import (
+	"context"
+	"log"
+	"os/exec"
+)
+
+const (
+	git   = "git"
+	clone = "clone"
+	pull  = "pull"
+)
+
+func GitClone(ctx context.Context, url, branch string, path string) error {
+	var cmd *exec.Cmd
+	if branch == "" {
+		cmd = exec.CommandContext(ctx, git, clone, url, path)
+	} else {
+		cmd = exec.CommandContext(ctx, git, clone, "-b", branch, url, path)
+	}
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Println(string(output))
+
+	return nil
+}
+
+func GitPull(ctx context.Context, path string) error {
+	cmd := exec.CommandContext(ctx, git, pull)
+	if path != "" {
+		cmd.Dir = path
+	}
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Println(string(output))
+
+	return nil
+}
